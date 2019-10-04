c41c83b8191aa9cf36b1a7661e4a75b8d3134ab5
Ikechukwu <iykekings36@gmail.com>
2019-10-04 09:14:28 +0100

    create method drive on GroundVehicle class

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,12 +3,14 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
     # TODO
-
+    def drive(self):
+        return "vroooom"
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -19,13 +21,13 @@ class GroundVehicle():
 
 # TODO
 
-vehicles = [
-    GroundVehicle(),
-    GroundVehicle(),
-    Motorcycle(),
-    GroundVehicle(),
-    Motorcycle(),
-]
+# vehicles = [
+#     GroundVehicle(),
+#     GroundVehicle(),
+#     Motorcycle(),
+#     GroundVehicle(),
+#     Motorcycle(),
+# ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
